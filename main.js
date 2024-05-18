453c9b91241819878b4accc787d38e1847abd55a
Thigas777 <152115239+Thigas777@users.noreply.github.com>
2024-05-18 11:56:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function preload() {
-
+  postimg.cc/jn4qVHcN
 }
 
 function setup() {
@@ -9,3 +9,16 @@ function setup() {
 function draw() {
     
 }
+
+function gotPoses(results){
+    if(results.length > 0){
+      console.log(results);
+      x = results[0].pose.nose.x;
+      y = results[0].pose.nose.y;
+      
+      }
+  }
+
+  function modelLoaded() {
+    console.log('PoseNet foi inicializado');
+ }
